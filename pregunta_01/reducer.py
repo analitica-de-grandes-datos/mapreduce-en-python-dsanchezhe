aa611ff1ec6ffbc9c80680581c0f5d5ca39a718e
dsanchezhe <dsanchezhe@unal.edu.co>
2023-06-13 19:28:46 -0500

    Punto1

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,45 +1,23 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-#! /usr/bin python
-
 import sys
 
-#
-# Esta funcion reduce los elementos que tienen la misma clave
-#
-if __name__ == '__main__':
+if __name__ == "__main__":
+  curkey = None
+  t = 0
 
-    curkey = None
-    total = 0
+  for line in sys.stdin:
+    key, val = line.split("\t")
+    val = int(val)
 
-    #
-    # cada linea de texto recibida es una entrada clave \tabulador valor
-    #
-    for line in sys.stdin:
+    if key == curkey:
+      t += val
 
-        key, val = line.split("\t")
-        val = int(val)
+    else:
+      if curkey is not None:
+        sys.stdout.write("{}\t{}\n".format(curkey, t))
+      curkey = key
+      t = val
 
-        if key == curkey:
-            #
-            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
-            # clave.
-            #
-            total += val
-        else:
-            #
-            # Se cambio de clave. Se reinicia el acumulador.
-            #
-            if curkey is not None:
-                #
-                # una vez se han reducido todos los elementos
-                # con la misma clave se imprime el resultado en
-                # el flujo de salida
-                #
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
-
-            curkey = key
-            total = val
-
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+  sys.stdout.write("{}\t{}\n".format(curkey, t))
