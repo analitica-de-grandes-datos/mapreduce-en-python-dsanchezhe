0082269e9443797179f69a6545b6056ce2566cd6
dsanchezhe <126032152+dsanchezhe@users.noreply.github.com>
2023-06-15 18:52:14 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,9 +1,25 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
 import sys
 
-for line in sys.stdin:
-    line = line.replace("\n","")
-    if '*' in line:
-        print(line.split("*")[1]+ "," +line.split("*")[0])
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    diccionario = {}
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        diccionario[key] = val
+
+    #ordernar el diccionario
+    diccionario = sorted(diccionario.items(), key=lambda x: x[1])
+
+    #imprimir el diccionario
+    for key, val in diccionario:
+        sys.stdout.write("{},{}\n".format(key, val))
