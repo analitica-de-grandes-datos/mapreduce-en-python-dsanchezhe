ce04c021b37a1c5e24e769dd99b17b093c26d987
dsanchezhe <126032152+dsanchezhe@users.noreply.github.com>
2023-06-15 18:52:48 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,16 +1,40 @@
-#
+##
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
-biggest_purposes = {}
 
-def set_bigger_purpose(dictionary_purposes, actual_element):
-    element_array = actual_element.split("*")
-    dictionary_purposes[element_array[0]] = int(dictionary_purposes.get(actual_element[0]) or 0) + 1 
-    return dictionary_purposes
+if __name__ == '__main__':
 
-for line in sys.stdin:
-    set_bigger_purpose(biggest_purposes, line)
+    curkey = None
+    total = 0
 
-for purpose, amount in biggest_purposes.items():
-    print(purpose + "," + str(amount))
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            total += val
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{},{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+
+    sys.stdout.write("{},{}\n".format(curkey, total))
